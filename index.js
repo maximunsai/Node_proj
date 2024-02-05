830f9c30aa3de11b44890e9bd6ad73765443bd95
Saikiran Nagulapuram <20274@arthahyd.com>
2024-02-05 09:57:49 +0530

    added error page with html

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const parser = require('body-parser');
 
 const adminRoute = require("./routers/admin");
@@ -13,7 +15,7 @@ app.use('/admin',adminRoute);
 app.use(shopRoute);
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>"Page not found"</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
